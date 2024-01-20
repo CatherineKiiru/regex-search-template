bf3316f57b733bd3bf346715b7b537c84512ac9f
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2024-01-20 17:29:53 +0300

    set up Axios and hooks component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { getPosts } from "./api/Axios";
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+  
+  useEffect(() => {
+
+  }, [])
+  const content = "";
+
+  return content;
+};
+
+export default App;
---
+++ src/api/Axios.jsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com",
+});
+
+export const getPosts = async () => {
+  // const api = axios.create({baseURL: "https://jsonplaceholder.typicode.com"})
+  const res = await api.get("/posts");
+  return res.data;
+};
